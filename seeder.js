f6f3f57531d392476abb75eae680104b9307997e
Lily Kong <lilyhk28@gmail.com>
2020-11-12 23:55:32 -0500

    finished successful seed file

--- seeder.js
+++ seeder.js
@@ -1,13 +1,24 @@
 const db = require('./database');
 
 //create a function to generate 100 records
+const seed = function() {
+  for(var i = 0; i < 100; i++) {
+    let data = [
+      Math.round((Math.floor(Math.random() * 2000000) + 100000) / 100) * 100,
+      Math.floor(Math.random() * 500)
+    ]
 
-let values = [100,50,5];
+    data.splice(1,0,Math.round(data[0]*.0069))
 
-db.insertOne(values, (err) => {
-  if (err) {
-    console.log(`error inserting data`, err)
-  } else {
-    console.log('inserted data')
+    db.insertOne(data, (err) => {
+      if (err) {
+        console.log(`error inserting data`, err)
+      } else {
+        console.log('inserted data')
+      }
+    })
   }
-})
+}
+
+seed();
+
