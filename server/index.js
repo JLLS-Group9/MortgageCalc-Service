604c9db67cd8752b8c8c347eab287a19024b09dc
Lily Kong <lilyhk28@gmail.com>
2020-11-12 22:54:08 -0500

    wrote successful api request

--- server/index.js
+++ server/index.js
@@ -1,13 +1,26 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const db = require('../database');
+
 const app = express();
 const PORT = 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
 app.get('/api/homes/:id/cost', (req, res) => {
-
+  console.log('req.params', req.params);
+  db.getOneCost(req.params.id, (err,data) => {
+    if (err) {
+      res.send(err)
+    } else {
+      res.send(data)
+    }
+  })
 })
 
 app.listen(PORT, () => {
