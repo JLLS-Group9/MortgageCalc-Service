96920fcae02c6b9b4f5b421b2404f7a7db10494f
Lily Kong <lilyhk28@gmail.com>
2020-11-14 14:18:08 -0500

    successful axios request to server

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import styles from '../style.css';
+import axios from 'axios';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      financials: {}
+    };
+    this.retrieveCost = this.retrieveCost.bind(this)
+  }
+
+  componentDidMount() {
+    this.retrieveCost(1);
+  }
+
+  //make a request for a given id
+  retrieveCost(id) {
+    axios.get(`/api/homes/${id}/cost`)
+      .then((data) => {
+        this.setState({financials: data.data[0]})
+      })
   }
 
   render() {
